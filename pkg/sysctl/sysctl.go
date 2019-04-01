9e5d81a84eadf3504e1ce15c5d17d4064ca87529
Jason-ZW <zhenyang@rancher.com>
2019-04-01 11:24:31 +0800

    Add sysctl settings

---
+++ pkg/sysctl/sysctl.go
@@ -0,0 +1,21 @@
+package sysctl
+
+import (
+	"io/ioutil"
+	"path"
+	"strings"
+
+	"github.com/niusmallnan/k3os/config"
+)
+
+func ConfigureSysctl(cfg *config.CloudConfig) error {
+	for k, v := range cfg.K3OS.Sysctl {
+		elements := []string{"/proc", "sys"}
+		elements = append(elements, strings.Split(k, ".")...)
+		path := path.Join(elements...)
+		if err := ioutil.WriteFile(path, []byte(v), 0644); err != nil {
+			return err
+		}
+	}
+	return nil
+}
