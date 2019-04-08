0a5c4b7811d743120eb05533a300b4330e11bc56
Jason-ZW <zhenyang@rancher.com>
2019-04-08 09:39:34 +0800

    Change proxy struct

--- pkg/network/proxy.go
+++ pkg/network/proxy.go
@@ -1,7 +1,6 @@
 package network
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,45 +10,39 @@ import (
 )
 
 func SettingProxy(cfg *config.CloudConfig) error {
-	address := cfg.K3OS.Network.Proxy.Address
+	httpProxy := cfg.K3OS.Network.Proxy.HttpProxy
+	httpsProxy := cfg.K3OS.Network.Proxy.HttpsProxy
 	noProxy := cfg.K3OS.Network.Proxy.NoProxy
-	protocol := cfg.K3OS.Network.Proxy.Protocol
-	if address != "" && protocol != "" {
-		proxyLines := make([]string, 0)
-		switch strings.ToLower(protocol) {
-		case "http":
-			for _, k := range []string{"HTTP_PROXY", "http_proxy"} {
-				proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, address))
-				if err := os.Setenv(k, address); err != nil {
-					return err
-				}
-			}
-			break
-		case "https":
-			for _, k := range []string{"HTTPS_PROXY", "https_proxy"} {
-				proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, address))
-				if err := os.Setenv(k, address); err != nil {
-					return err
-				}
-			}
-			break
-		default:
-			return errors.New("proxy's protocol not match http or https")
-		}
-		if len(noProxy) > 0 {
-			for _, k := range []string{"NO_PROXY", "no_proxy"} {
-				proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, noProxy))
-				if err := os.Setenv(k, address); err != nil {
-					return err
-				}
-			}
-		}
-		if len(proxyLines) > 0 {
-			proxy := strings.Join(proxyLines, "\n")
-			proxy = fmt.Sprintf("#!/bin/sh\n%s\n", proxy)
-			if err := ioutil.WriteFile("/etc/profile.d/proxy.sh", []byte(proxy), 0644); err != nil {
+	proxyLines := make([]string, 0)
+	if len(httpProxy) > 0 {
+		for _, k := range []string{"HTTP_PROXY", "http_proxy"} {
+			proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, httpProxy))
+			if err := os.Setenv(k, httpProxy); err != nil {
 				return err
 			}
+		}
+	}
+	if len(httpsProxy) > 0 {
+		for _, k := range []string{"HTTPS_PROXY", "https_proxy"} {
+			proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, httpsProxy))
+			if err := os.Setenv(k, httpsProxy); err != nil {
+				return err
+			}
+		}
+	}
+	if len(noProxy) > 0 {
+		for _, k := range []string{"NO_PROXY", "no_proxy"} {
+			proxyLines = append(proxyLines, fmt.Sprintf("export %s=%s", k, noProxy))
+			if err := os.Setenv(k, noProxy); err != nil {
+				return err
+			}
+		}
+	}
+	if len(proxyLines) > 0 {
+		proxy := strings.Join(proxyLines, "\n")
+		proxy = fmt.Sprintf("#!/bin/sh\n%s\n", proxy)
+		if err := ioutil.WriteFile("/etc/profile.d/proxy.sh", []byte(proxy), 0644); err != nil {
+			return err
 		}
 	}
 	return nil
