2c8168269c2855c8af330e7c7feefb988ddf39f0
Jason-ZW <zhenyang@rancher.com>
2019-04-19 16:14:08 +0800

    Add ssh host_keys

--- config/schema.go
+++ config/schema.go
@@ -113,6 +113,9 @@ var schema = `{
         "daemon": {
           "type": "boolean"
         },
+        "host_keys": {
+          "type": "object"
+        },
         "port": {
           "type": "integer"
         }
