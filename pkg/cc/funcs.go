896c419b5213bd26b87ff9670235097e2246fbde
Jacob Blain Christen <jacob@rancher.com>
2020-10-02 01:27:22 -0700

    k3s: avoid over-quoting install envvars (#590)
    Fix K3S_URL, K3S_TOKEN, and K3S_CLUSTER_SECRET being passed to k3s
    install.sh with extra quotes around the values. Rely on k3s install.sh
    to correctly capture entries for `/etc/k3s/k3s-service.env`.
    
    Fixes #589

--- pkg/cc/funcs.go
+++ pkg/cc/funcs.go
@@ -122,16 +122,16 @@ func ApplyK3S(cfg *config.CloudConfig, restart, install bool) error {
 			args = append(args, "server")
 		}
 	} else {
-		vars = append(vars, fmt.Sprintf("K3S_URL=\"%s\"\n", cfg.K3OS.ServerURL))
+		vars = append(vars, fmt.Sprintf("K3S_URL=%s", cfg.K3OS.ServerURL))
 		if len(args) == 0 {
 			args = append(args, "agent")
 		}
 	}
 
 	if strings.HasPrefix(cfg.K3OS.Token, "K10") {
-		vars = append(vars, fmt.Sprintf("K3S_TOKEN=\"%s\"\n", cfg.K3OS.Token))
+		vars = append(vars, fmt.Sprintf("K3S_TOKEN=%s", cfg.K3OS.Token))
 	} else if cfg.K3OS.Token != "" {
-		vars = append(vars, fmt.Sprintf("K3S_CLUSTER_SECRET=\"%s\"\n", cfg.K3OS.Token))
+		vars = append(vars, fmt.Sprintf("K3S_CLUSTER_SECRET=%s", cfg.K3OS.Token))
 	}
 
 	var labels []string
