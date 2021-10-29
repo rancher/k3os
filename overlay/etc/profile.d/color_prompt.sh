190915fd16e79fb884b1891c35ba2fb51e2d3a5e
Thomas Riccardi <riccardi.thomas@gmail.com>
2021-10-29 15:20:18 +0200

    shell prompt: no color on dumb terminals
    It notably fixes emacs tramp access to k3os, see https://stackoverflow.com/a/8363532/15151442

--- overlay/etc/profile.d/color_prompt.sh
+++ overlay/etc/profile.d/color_prompt.sh
@@ -1,8 +1,15 @@
-# Setup a red prompt for root and a green one for users.
-# rename this file to color_prompt.sh to actually enable it
-NORMAL="\[\e[0m\]"
-RED="\[\e[1;31m\]"
-GREEN="\[\e[1;32m\]"
+# Setup a red prompt for root and a green one for users, on non-dumb terminals.
+
+case "$TERM" in
+"dumb")
+	;;
+xterm*|rxvt*|eterm*|screen*)
+	NORMAL="\[\e[0m\]"
+	RED="\[\e[1;31m\]"
+	GREEN="\[\e[1;32m\]"
+	;;
+esac
+
 if [ "$(id -u)" = 0 ]; then
 	PS1="$RED\h [$NORMAL\w$RED]# $NORMAL"
 else
